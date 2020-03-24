8ea6d9a55fe54b964f50875ad025b0a6c4f3d9ed
Ben Weakley <2173281+Defying@users.noreply.github.com>
2020-03-24 16:29:10 -0400

    pi 4 updates

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,10 +3,10 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-killall -q compton
-while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+#killall -q compton
+#while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
 
 echo -e "-- begin --\n" | tee -a /tmp/polybar.log | tee -a /tmp/compton.log
 
 polybar top >>/tmp/polybar.log 2>&1 &
-compton >>/tmp/compton.log 2>&1 &
+#compton >>/tmp/compton.log 2>&1 &
